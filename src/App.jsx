cc47b88b6c4c02f1c01c4788ff519d277e8b4014
miriamrami <miriamteresaram@gmail.com>
2025-02-07 16:23:21 -0500

    added functionality to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import Home from './components/Home';
+import Fortune from './components/Fortune';
+import React, {useState} from 'react';
 
 function App() {
+  const [showFortune, setShowFortune] = useState(false);
+
+  const handleAccept = () => {
+    setShowFortune(true);
+  };
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      {showFortune ? <Fortune /> : <Home onAccept={handleAccept} />}
     </div>
   );
 }
